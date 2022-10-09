0762d0a5121c5d72f18e9cd6e4b29f4993dbf78e
jcuetocalnick <100786631+jcuetocalnick@users.noreply.github.com>
2022-10-08 20:24:46 -0400

    made changes to html & javascript for better functionality

--- loginscreen.js
+++ loginscreen.js
@@ -1,4 +1,19 @@
+function login(){
+   x.style.left="50px";
+   y.style.left="450px";
+   z.style.left="0px";
 
- 
+   var username1=document.getElementById("username").value;
+   var password1=document.getElementById("password").value;
+   
+}
+function register(){
+   x.style.left="-400px";
+   y.style.left="50px";
+   z.style.left="110px";
 
+   var username2=document.getElementById("username").value;
+   var password2=document.getElementById("password").value;
+}
+   
 
